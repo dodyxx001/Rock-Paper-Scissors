cb8763b605128ff8d22b50c3b06da87ce0014b03
Ziga Dodic <ziga.dodic@gmail.com>
2021-11-25 12:46:22 +0100

    Second version - 5 rounds of game functional

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Function that returns a random result (computer's choice):
+// Result counter (each win adds 1 to the variable)
 
+let playerCounter = 0;
+let computerCounter = 0;
+
+// Main function that includes one round of a game:
+function oneRound() {
+
+// Function that returns a random result (computer's choice):
 let computerPlay = () => {
     let result = Math.floor(Math.random() * 3);
     if (result === 0){
@@ -12,13 +19,13 @@ let computerPlay = () => {
 };
 
 // Getting the user's input and lowerCasing it
-let userInput = prompt().toLowerCase();
+let userInput = prompt("Please enter rock, paper or scissors").toLowerCase();
 
 // Saving the inputs to variables
 let computerSelection = computerPlay();
 let playerSelection = userInput;
 
-// Round of game
+// Function that compares inputs and declares the winner
 let round = (computerSelection, playerSelection) => {
   
   let winText = 'Congratulations, you win!';
@@ -40,20 +47,46 @@ let round = (computerSelection, playerSelection) => {
       return loseText;
   };
 };
-  
-let playerCounter = 0;
-let computerCounter = 0;
 
+// Adding to the counters of results:
 if(round(computerSelection, playerSelection) === 'Congratulations, you win!'){
     playerCounter++;
 }else if(round(computerSelection, playerSelection) === 'Sorry, you lose!'){
     computerCounter++;
 };
 
+// Returning the results to user
 
-
-alert(computerSelection);
+alert('The computer choice was: ' + computerSelection + ' -- ' + round(computerSelection, playerSelection));
 console.log(round(computerSelection, playerSelection));
+
+// end of game-round main function
+};
+
+
+
+// Repeating rounds 5 times
+for( let i = 0; i <= 5; i++){
+    if(i){oneRound()};
+};
+
+// Counting the results of 5 rounds and declaring the winner:
+
+if (playerCounter > computerCounter){
+    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. You win the game!'`);
+}else if (playerCounter < computerCounter){
+    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. You lose the game!'`);
+}else if (playerCounter === computerCounter){
+    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. The results are tied. Reload the page.'`)
+};
+
+
+
+
+
+
+
+
 
 
 
