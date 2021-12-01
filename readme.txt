b85bccd371eea10a1e354d84f725b5ffaf81d4e4
Ziga Dodic <ziga.dodic@gmail.com>
2021-12-01 07:40:59 +0100

    Third version - added UI

---
+++ readme.txt
@@ -0,0 +1,41 @@
+This is a project made entirely by myself, as a part of The Odin Project course.
+The goals of this projects were:
+
+ - for the first version: create a console-based five-round game, with no user UI,
+   that prompts user for input, than randomly selects computer input,
+   runs the MAIN game function which compares the inputs and returns
+   (alerts) the user with results.
+ - version 1 has only 1 round of the game functional.
+ - version 2 has 5 rounds of game functional.
+
+Next step was to create a user UI with buttons.
+Clicking the buttons gets the user's input. Computer's input is generated randomly.
+This is saved in version 3:
+
+- User interface, made with HTML and CSS
+- Animated buttons (with CSS and JS), which change on hover, and on click, they
+  fetch the user's input.
+- Animated display of results (animations done with CSS and JS - eventlisteners
+  and keyframes).
+- An animated field, which shows the win-text or lose-text, so the player gets the
+  information if he won or lost the current round.
+- A result-counter at the bottom of the page, which displays current score of 
+  both players.
+- A final-result window, which shows when one of the players reaches score of 3
+- This window is positioned in center of the page and everything else is blurred, so 
+  player cannot continue the game.
+- A 'play again' button is added with 'reload page' function, so player can play 
+  again.
+
+
+This project was quite fun to make and quite challenging for a beginner! :)
+Things I learned:
+- Keeping the code organised, because it got long and I needed logic
+- Writing a lot of comments
+- JS and the DOM, targeting HTML elements, adding various eventlisteners
+- Animations in CSS ( keyframes ).
+- Adding and removing classes
+- Timeout function and its use
+- Creating and appending child html elements
+- Blurring the background
+- A little bit longer and complex functions:)
--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// Result counter (each win adds 1 to the variable)
+// Assinging an event listeners to each logo
+// Adding a 'clicked' class, then firing the oneRound function, then
+// removing the 'clicked 'class - this action gets the user's input.
 
+let buttons = document.querySelectorAll('.choice');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+    e.target.classList.add('clicked');
+    oneRound();
+    e.target.classList.remove('clicked');
+});
+});
+
+
+// Result counter (each win adds 1 to the variable)
 let playerCounter = 0;
 let computerCounter = 0;
 
-// Main function that includes one round of a game:
+
+// MAIN FUNCTION that includes one round of a game:
+// _________________________________________________________________
 function oneRound() {
+
 
 // Function that returns a random result (computer's choice):
 let computerPlay = () => {
@@ -18,20 +35,55 @@ let computerPlay = () => {
     };
 };
 
-// Getting the user's input and lowerCasing it
-let userInput = prompt("Please enter rock, paper or scissors").toLowerCase();
+
+// Getting the user's input  (by 'clicked' class)
+let choice = document.getElementsByClassName('clicked')[0].id
+let playerInput = choice;
 
 
 // Saving the inputs to variables
-
 let computerSelection = computerPlay();
-let playerSelection = userInput;
+let playerSelection = playerInput;
 
-// Function that compares inputs and declares the winner
+
+// Printing the choices in the DOM HTML:
+let computerChoiceParagraph = document.querySelector('#computer-choice');
+computerChoiceParagraph.textContent = computerSelection;
+
+let playerChoiceParagraph = document.querySelector('#player-choice');
+playerChoiceParagraph.textContent = playerSelection;
+
+
+// Animating the background of choices in HTML:
+if(playerChoiceParagraph.textContent){
+    playerChoiceParagraph.classList.add('showPlayerChoice');
+};
+if(computerChoiceParagraph.textContent){
+    computerChoiceParagraph.classList.add('showComputerChoice');
+};
+
+// Removing classes from ShowResults section so they can animate again:
+let allChoices = document.querySelectorAll('.choice');
+let yourChoice = document.getElementById('player-choice');
+let computerChoice = document.getElementById('computer-choice');
+// This timeout function removes and adds again the class to repeat the animations:
+allChoices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+        yourChoice.classList.remove('showPlayerChoice');
+        computerChoice.classList.remove('showComputerChoice');
+        setTimeout(function() {
+            yourChoice.classList.add('showPlayerChoice');
+            computerChoice.classList.add('showComputerChoice');
+        }, 0.01);
+        
+    });
+});
+
+// Function that takes in and compares inputs and declares the winner
 let round = (computerSelection, playerSelection) => {
   
-  let winText = 'Congratulations, you win!';
-  let loseText = 'Sorry, you lose!';
+  let winText = 'You win the round!';
+  let loseText = 'You lose the round!';
 
   if (computerSelection === playerSelection){
       return 'It\'s a tie!';
@@ -50,37 +102,89 @@ let round = (computerSelection, playerSelection) => {
   };
 };
 
+
+// Returning the results to user - wintext paragraph
+let winTextParagraph = document.querySelector('#wintext');
+winTextParagraph.textContent = round(computerSelection, playerSelection);
+
+
+// Animating the background of wintext:
+let winText = document.querySelector('#wintext');
+if(winText.textContent){
+    winText.classList.add('showWinText');
+};
+
+
 // Adding to the counters of results:
-if(round(computerSelection, playerSelection) === 'Congratulations, you win!'){
+if(round(computerSelection, playerSelection) === 'You win the round!'){
     playerCounter++;
-}else if(round(computerSelection, playerSelection) === 'Sorry, you lose!'){
+}else if(round(computerSelection, playerSelection) === 'You lose the round!'){
     computerCounter++;
 };
 
-// Returning the results to user
 
-alert('The computer choice was: ' + computerSelection + ' -- ' + round(computerSelection, playerSelection));
-console.log(round(computerSelection, playerSelection));
+// Printing the current value of win-counters in the HTML DOM:
+let playerCounterInHtml = document.querySelector('#player-score');
+playerCounterInHtml.textContent = playerCounter;
 
-// end of game-round main function
+let computerCounterInHtml = document.querySelector('#computer-score');
+computerCounterInHtml.textContent = computerCounter;
+
+
+// Announcement of results in the announcement section - shows only when game reaches 3 rounds:
+let announceDiv = document.querySelector('.announce');
+let announceWin = document.querySelector('#announce-win');
+let announceLose =document.querySelector('#announce-lose');
+
+if(playerCounter >= 3 || computerCounter >= 3){
+    announceDiv.classList.add('showAnnouncement');
+    let announceScoresParagraph = document.createElement('p');
+    announceScoresParagraph.textContent = `Score: You: ${playerCounter}, Comp: ${computerCounter}`;
+    announceDiv.appendChild(announceScoresParagraph);
+}
+
+if(playerCounter >= 3){
+    announceWin.classList.add('showWin');
+}
+
+if(computerCounter >= 3){
+    announceLose.classList.add('showLose');
+}
+
+
+// Adding blur to the body when announcement shows:
+let bodyBlur = document.querySelector('body');
+if(playerCounter >= 3 || computerCounter >= 3){
+    bodyBlur.classList.add('bodyBlur');
+}
+
+
+// END OF game-round main function
+// _________________________________________________________________
 };
 
 
+// Animating the text under logo-buttons:
+let textUnderButtons = document.querySelectorAll('.choice img');
 
-// Repeating rounds 5 times / if i is true, play oneround
-for( let i = 0; i <= 5; i++){
-    if(i){oneRound()};
-};
+textUnderButtons.forEach((btn) => {
+    btn.addEventListener('mouseover', (e) => {
+        e.target.parentElement.lastElementChild.classList.add('hovering');
+    });
+    btn.addEventListener('mouseout', (e) => {
+        e.target.parentElement.lastElementChild.classList.remove('hovering');
+    });
+});
 
-// Counting the results of 5 rounds and declaring the winner:
 
-if (playerCounter > computerCounter){
-    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. You win the game!'`);
-}else if (playerCounter < computerCounter){
-    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. You lose the game!'`);
-}else if (playerCounter === computerCounter){
-    alert(`'Your result is ${playerCounter} and the computers result is ${computerCounter}. The results are tied. Reload the page.'`)
-};
+// Adding reloading function to PLAY AGAIN button:
+let playAgainButton = document.querySelector('#play-again');
+
+playAgainButton.addEventListener('click', () => {
+    location.reload();
+})
+
+
 
 
 
